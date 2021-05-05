2589dc22e5a7c23c3e62195d13bb1b1cd9bcf0f2
Julie <julie3667022@gmail.com>
2021-05-05 21:34:31 +0800

    python

---
+++ 上課資源/file.py
@@ -0,0 +1,8 @@
+上課資源 :
+https://drive.google.com/drive/folders/1RNM2Ye-dL1mKK3FxYRQ3JGuxfnNZvBgo?usp=sharing
+
+Github 範例程式
+https://github.com/vincenttuan/yzu_python_20210414
+
+Python Operator Precedence
+https://www.mathcs.emory.edu/~valerie/courses/fall10/155/resources/op_precedence.html
